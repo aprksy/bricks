1dc6c2fbe29de84bfbc17b83055ec4927d99b970
Agung Prakasya <aprksy@gmail.com>
2024-06-27 23:43:57 +0700

    add onInfo capability

--- base/identity/impl.go
+++ base/identity/impl.go
@@ -4,16 +4,18 @@ import "fmt"
 
 var _ Identity[int] = (*SimpleIdentity[int])(nil)
 
-func NewSimpleIdentity[T comparable](id T, typeName string) *SimpleIdentity[T] {
+func NewSimpleIdentity[T comparable](id T, typeName string, onInfo func(t string, id T) string) *SimpleIdentity[T] {
 	return &SimpleIdentity[T]{
 		id:       id,
 		typeName: typeName,
+		onInfo:   onInfo,
 	}
 }
 
 type SimpleIdentity[T comparable] struct {
 	id       T
 	typeName string
+	onInfo   func(t string, id T) string
 }
 
 // Id implements Identity.
@@ -28,5 +30,8 @@ func (s *SimpleIdentity[T]) TypeName() string {
 
 // InstanceInfo implements Identity.
 func (s *SimpleIdentity[T]) InstanceInfo() string {
+	if s.onInfo != nil {
+		return s.onInfo(s.typeName, s.id)
+	}
 	return fmt.Sprintf("%s (%v)", s.typeName, s.id)
 }
