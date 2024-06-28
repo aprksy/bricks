fda1b8c685d3ddada4deab89c448ad8081daae6a
Agung Prakasya <aprksy@gmail.com>
2024-06-28 22:41:12 +0700

    change subject/observer interface

--- base/pattern/observer/intf.go
+++ base/pattern/observer/intf.go
@@ -1,17 +1,28 @@
 package observer
 
-type Subject[T comparable] interface {
-	Get() T
-	Inject(value T)
-	Add(obs Observer[T]) (subsId string, err error)
-	Remove(subsId string) error
+import "github.com/aprksy/bricks/base/identity"
+
+/*
+	- Subject can only be a dispatcher of a single domain value (slice length, circle radius etc.)
+	- key is the name of the value it holds (being dispatched to observers)
+*/
+
+type Subject[I, T comparable] interface {
+	identity.Identity[I]
+	Supports(key string) bool
+	Extract() T
+	Inject(value T) error
+	Add(obs Observer[I, T]) (*I, error)
+	Remove(subsId I) error
 	Notify()
 }
 
-type Observer[T comparable] interface {
-	Subscribe(subj Subject[T], localId string) (subsId *string, err error)
-	Unsubscribe(subsId string) error
-	Ready(subsId string)
-	Receive(subsId string, value T)
+type Observer[I, T comparable] interface {
+	identity.Identity[I]
+	SubscribeByKey(key string) (*I, Subject[I, T], error)
+	Subscribe(subject Subject[I, T], key string) (*I, error)
+	Unsubscribe(subsId I) error
+	Ready(subsId I)
+	Receive(subsId I, value T)
 	Extract(key string) (*T, error)
 }
