479fecfa8caf0a241bb34b3bf766a76ba19b08be
Agung Prakasya <aprksy@gmail.com>
2024-06-28 22:41:37 +0700

    add new observer implementation

--- base/pattern/observer/impl-observer.go
+++ base/pattern/observer/impl-observer.go
@@ -3,86 +3,103 @@ package observer
 import (
 	"fmt"
 	"sync"
+
+	"github.com/aprksy/bricks/base/identity"
 )
 
-var _ Observer[int] = (*SimpleObserver[int])(nil)
+var _ Observer[int, int] = (*SimpleObserver[int, int])(nil)
 
-func NewSimpleObserver[T comparable](onReceive func(localId string, value T)) *SimpleObserver[T] {
-	return &SimpleObserver[T]{
-		states:        map[string]T{},
-		subscriptions: map[string]Subscription[T]{},
-		OnReceive:     onReceive,
+func NewSimpleObserver[I, T comparable](oid I, onReceive func(key string, value T)) *SimpleObserver[I, T] {
+	id := identity.NewSimpleIdentity[I](oid, "simple-observer", nil)
+	return &SimpleObserver[I, T]{
+		SimpleIdentity: *id,
+		subscriptions:  map[I]Subject[I, T]{},
+		subsByKey:      map[string]I{},
+		dataByKey:      map[string]T{},
+		keysBySub:      map[I]string{},
+		OnReceive:      onReceive,
 	}
 }
 
-type SimpleObserver[T comparable] struct {
+type SimpleObserver[I, T comparable] struct {
+	identity.SimpleIdentity[I]
 	mutex         sync.Mutex
-	states        map[string]T
-	subscriptions map[string]Subscription[T]
-	OnReceive     func(localId string, value T)
+	subscriptions map[I]Subject[I, T]
+	keysBySub     map[I]string
+	subsByKey     map[string]I
+	dataByKey     map[string]T
+	OnReceive     func(key string, value T)
+}
+
+// Extract implements Observer.
+func (s *SimpleObserver[I, T]) Extract(key string) (*T, error) {
+	value, exists := s.dataByKey[key]
+	if !exists {
+		return nil, fmt.Errorf(ErrKeyNotFound)
+	}
+	return &value, nil
+}
+
+// Ready implements Observer.
+func (s *SimpleObserver[I, T]) Ready(subsId I) {
+	s.subscriptions[subsId].Notify()
 }
 
 // Receive implements Observer.
-func (s *SimpleObserver[T]) Receive(subsId string, value T) {
+func (s *SimpleObserver[I, T]) Receive(subsId I, value T) {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
 
-	localId := s.subscriptions[subsId].LocalId
-	s.states[localId] = value
+	key := s.keysBySub[subsId]
+	s.dataByKey[key] = value
 	if s.OnReceive != nil {
-		s.OnReceive(localId, value)
+		s.OnReceive(key, value)
 	}
 }
 
 // Subscribe implements Observer.
-func (s *SimpleObserver[T]) Subscribe(subj Subject[T], localId string) (subsId *string, err error) {
-	err = fmt.Errorf(ErrSubjectNil)
-	if subj == nil {
+func (s *SimpleObserver[I, T]) Subscribe(subject Subject[I, T], key string) (*I, error) {
+	err := fmt.Errorf(ErrSubjectNil)
+	if subject == nil {
 		return nil, err
 	}
 
-	if id, err := subj.Add(s); err != nil {
+	subsid, err := subject.Add(s)
+	if err != nil {
 		return nil, err
-	} else {
-		subsId = &id
 	}
-	defer s.Ready(*subsId)
 
-	subs := Subscription[T]{
-		LocalId: localId,
-		Subject: subj,
-	}
+	defer s.Ready(*subsid)
 
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
-	s.subscriptions[*subsId] = subs
+	s.subscriptions[*subsid] = subject
+	s.subsByKey[key] = *subsid
+	s.keysBySub[*subsid] = key
 
-	return subsId, nil
+	return subsid, nil
+}
+
+// SubscribeByKey implements Observer.
+func (s *SimpleObserver[I, T]) SubscribeByKey(key string) (*I, Subject[I, T], error) {
+	panic("unimplemented")
 }
 
 // Unsubscribe implements Observer.
-func (s *SimpleObserver[T]) Unsubscribe(subsId string) error {
-	subs, exists := s.subscriptions[subsId]
+func (s *SimpleObserver[I, T]) Unsubscribe(subsId I) error {
+	subject, exists := s.subscriptions[subsId]
 	if !exists {
 		return fmt.Errorf(ErrSubscriptionNotFound)
 	}
 
-	delete(s.states, subs.LocalId)
-	delete(s.subscriptions, subsId)
-	return nil
-}
-
-// Ready implements Observer.
-func (s *SimpleObserver[T]) Ready(subsId string) {
-	s.subscriptions[subsId].Subject.Notify()
-}
-
-// Extract implements Observer.
-func (s *SimpleObserver[T]) Extract(key string) (*T, error) {
-	value, exists := s.states[key]
-	if !exists {
-		return nil, fmt.Errorf(ErrKeyNotFound)
+	if err := subject.Remove(subsId); err != nil {
+		return err
 	}
 
-	return &value, nil
+	key := s.keysBySub[subsId]
+	delete(s.keysBySub, subsId)
+	delete(s.subsByKey, key)
+	delete(s.dataByKey, key)
+	delete(s.subscriptions, subsId)
+	return nil
 }
